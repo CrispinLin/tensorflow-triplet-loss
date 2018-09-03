cf39a6ec70ae7a297feef62b6cd89f86e72f5453
CrispinLin <nivose123@163.com>
2018-09-03 14:42:54 +0200

    reduce learning rate

--- model/input_fn.py
+++ model/input_fn.py
@@ -13,7 +13,8 @@ def train_input_fn(data_dir, params):
         params: (Params) contains hyperparameters of the model (ex: `params.num_epochs`)
     """
     dataset = mnist_dataset.train(data_dir)
-    # dataset = dataset.shuffle(params.train_size)  # whole dataset into the buffer
+    # dataset = dataset.shuffle(
+    #     params.train_size)  # whole dataset into the buffer
     dataset = dataset.repeat(params.num_epochs)  # repeat for multiple epochs
     dataset = dataset.batch(params.batch_size)
     dataset = dataset.prefetch(
